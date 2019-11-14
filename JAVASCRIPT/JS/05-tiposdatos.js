ea77dbd1a353f01f337f12e2737259a4136ef9f9
pipe <pipef104@gmail.com>
2019-11-14 10:13:40 -0500

    It's Added Operadores.js

---
+++ JAVASCRIPT/JS/05-tiposdatos.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// operadores 
+var num1 = 7;
+var num2 = 12;
+var oparacion = num1 + num2;
+
+alert("El resultado de operación es : " + oparacion);
+
+//tipos de datos
+var numero_entero = 44;
+var cadena_texto = "Hola 'que'  tal";
+var verdadero = true; //Tipo de dato boolean
+var falso = false; //Tipo de dato boolean
+
+console.log(cadena_texto);

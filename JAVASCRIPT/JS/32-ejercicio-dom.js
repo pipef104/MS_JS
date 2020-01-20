8865025fead828e2efcae15b1201fa8d6b2b1135
Felipe <pipef104@gmail.com>
2020-01-20 10:19:36 -0500

    Upload more files

--- JAVASCRIPT/JS/32-ejercicio-dom.js
+++ JAVASCRIPT/JS/32-ejercicio-dom.js
@@ -9,16 +9,33 @@ window.addEventListener("load", () => {
   formulario.addEventListener("submit", () => {
     console.log("Evento Submit Capturado");
 
-  var nombre = document.querySelector("#nombre").value;
-  var apellidos = document.querySelector("#apellidos").value;
-  var edad=  document.querySelector("#edad").value;
-  box_dashed.style.display = "block";
-  var datos_usuario = [nombre, apellidos, edad];
-  var indice;
-  for (indice in datos_usuario) {
-    var parrafo = document.createElement("p");
-    parrafo.append(datos_usuario[indice]);
-    box_dashed.append(parrafo);
-  }
+    var nombre = document.querySelector("#nombre").value;
+    var apellidos = document.querySelector("#apellidos").value;
+    var edad = parseInt(document.querySelector("#edad").value);
+
+    if (nombre.trim() == null || nombre.trim().length == 0) {
+      alert("El nombre es valido");
+      return false;
+
+    }
+    if (apellidos.trim() == null || apellidos.trim().length == 0) {
+      alert("Los apellidos no son validos");
+      return false;
+
+    }
+    if (edad == null || edad <= 0 || isNaN(edad)) {
+      alert("La edad no es valida");
+      return false;
+
+    }
+
+    box_dashed.style.display = "block";
+    var datos_usuario = [nombre, apellidos, edad];
+    var indice;
+    for (indice in datos_usuario) {
+      var parrafo = document.createElement("p");
+      parrafo.append(datos_usuario[indice]);
+      box_dashed.append(parrafo);
+    }
   });
 });
---
+++ JAVASCRIPT/JS/33-Json.js
@@ -0,0 +1,26 @@
+'use strict'
+
+//JSON - Java Script Object Notation
+
+var pelicula = {
+    titulo: 'Batman VS SuperMan',
+    year: 2017,
+    pais: 'Estados Unidos'
+};
+
+var peliculas = [
+    {
+        titulo: "La verdad Duele",
+        year: 2016,
+        pais: 'Francia'
+    },
+    pelicula
+];
+var caja_peliculas = document.querySelector("#peliculas");
+
+var index;
+for (index in peliculas) {
+   var p = document.createElement("p");
+   p.append(peliculas[index].titulo + "-" + peliculas[index].year);
+   caja_peliculas.append(p);
+}
